7f3f3e50a1ee0acdd35bf7600e245b81bccfa714
patapata23 <sinner315@gmail.com>
2025-10-15 19:50:18 +0900

    エラーの修正4

--- src/sample.blade.php
+++ src/sample.blade.php
@@ -16,8 +16,8 @@
 </table>
 </div>
 
-{{-- @foreach ($plans as $idx => $plan)
+@foreach ($plans as $idx => $plan)
 <tr>
 <td class="txt_center"><input type="checkbox" name="ldgid[]" value="{{{$plan->ldgid}}}" class="ldgid" data-total="{{{$plan->total}}}" /></td>
 <td>{{{$plan->service }}}</td>
-@endforeach --}}
+@endforeach
