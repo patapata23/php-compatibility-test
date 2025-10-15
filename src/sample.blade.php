dca0cc65d97cb710493623c65e9090368da59afe
patapata23 <sinner315@gmail.com>
2025-10-15 19:48:54 +0900

    エラーの修正3

--- src/sample.blade.php
+++ src/sample.blade.php
@@ -1,4 +1,20 @@
 
+<div class="form_box">
+
+<table class="list">
+<tr>
+<th width="20" class="txt_center"><input type="checkbox" id="checkAll" /></th>
+<th width="70">サービス</th>
+<th width="60">相談ID</th>
+<th width="120">依頼者名</th>
+<th width="120">かな</th>
+<th width="100">予定日</th>
+<th width="100">摘要</th>
+<th width="100">出金額</th>
+<th> </th>
+</tr>
+</table>
+</div>
 
 {{-- @foreach ($plans as $idx => $plan)
 <tr>
