808f63df1b44dde36c4e297dfc8d1b8665935813
patapata23 <sinner315@gmail.com>
2025-10-15 19:33:20 +0900

    エラーの修正

--- src/sample.blade.php
+++ src/sample.blade.php
@@ -19,5 +19,4 @@
 <tr>
 <td class="txt_center"><input type="checkbox" name="ldgid[]" value="{{{$plan->ldgid}}}" class="ldgid" data-total="{{{$plan->total}}}" /></td>
 <td>{{{$plan->service }}}</td>
-$('#edit_{{{ $ptype} ?? '' }}').css('display', 'block');
 @endforeach
